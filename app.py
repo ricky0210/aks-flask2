94fa1595da9382ebb108f314f114aaec2357115e
Ricky Choi <cys8484@gmail.com>
2020-04-17 16:40:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     합계 = sum(range(100000000))
-    return "서버 호스트 : {}, 합계 : {}".format(platform.node(), 합계)
+    return "서버 호스트  :: {}, 합계 : {}".format(platform.node(), 합계)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
